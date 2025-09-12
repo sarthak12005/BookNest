b9389d6869f528bd079826d02524a4918105df2b
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-09-12 21:05:33 +0530

    added some changes

---
+++ be/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+    res.send("Server is running..");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+})
