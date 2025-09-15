e856608cd548e09db1f126f162d4965d98fddb2b
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-09-15 21:25:54 +0530

    added some changes

--- be/src/routes/auth.routes.js
+++ be/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {loginUser} = require('../controllers/auth.controller');
+const {loginUser, registerUser} = require('../controllers/auth.controller');
 
 
 router.post('/login', loginUser);
+router.post('/signup', registerUser);
 
 
 
