adc845b01a3f9fdabb53a1cdae2d613cb1514f4d
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-09-16 20:49:00 +0530

    created login controller

--- be/src/controllers/auth.controller.js
+++ be/src/controllers/auth.controller.js
@@ -4,10 +4,37 @@ const { generateToken } = require('../lib/jwt');
 
 
 exports.loginUser = async (req, res) => {
-     const { email, password } = req.body;
+    try {
+          const {email, password} = req.body;
 
-     console.log('the email is : ' + email);
-     console.log('the password is : ' + password);
+          if (!email || !password) {
+               return res.status(400).json({message:"Credentials required"});
+          }
+
+          const user = await User.findOne({email});
+
+          if (!user) {
+               return res.status(404).json({message:"User not found "});
+          }
+
+          const validated = await compareHashPass(password, user.password);
+
+          if (!validated) {
+               return res.status(401).json({message: "incorrect password or Email"});
+          }
+
+          const token = await generateToken(user._id, process.env.JWT_SECRET, process.env.JWT_EXPIRY);
+
+          if (!token ) {
+               return res.status(404).json({message: "Failed to generate token "});
+          }
+
+          res.status(200).json({message:"User login Successfully", user});
+
+    } catch (err) {
+       console.log("Error in login user", err);
+       res.status(500).json({message: "Internal Server Error"});
+    }
 }
 
 exports.registerUser = async (req, res) => {
