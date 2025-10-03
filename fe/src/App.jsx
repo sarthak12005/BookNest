03f83f0fa8ed23041692714b00cd83adca3c1ab0
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-10-03 16:33:39 +0530

    added some changes

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,18 +3,25 @@ import Home from './pages/Home'
 import Books from './pages/Books'
 import Categories from './pages/Categories'
 import AuthPage from './pages/AuthPage'
+import toast, {Toaster} from 'react-hot-toast';
 import './App.css'
 
 function App() {
+
     return (
       <>
          <div >
+
+          {/* initializing the Routes */}
            <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/books' element={<Books />} />
               <Route path='/categories' element={<Categories/>} />
               <Route path='/auth' element={<AuthPage/>} />
            </Routes>
+
+           {/* initializing the toaster  */}
+           <Toaster position='top-right' reverseOrder={false} />
          </div>
       </>
     );
