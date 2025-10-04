fcaccca2fc94dca28b7896ef3b2961d10a0c621e
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-10-04 18:07:54 +0530

    added some changes

--- fe/src/components/Sidebar.jsx
+++ fe/src/components/Sidebar.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import { Search, SearchIcon, ShoppingCart } from 'lucide-react'
 
 const Sidebar = () => {
+  const menuItems = [
+    { name: 'Home', link: '/' },
+    { name: 'Books', link: '/books' },
+    { name: 'Categories', link: '/categories' },
+    { name: 'Author', link: '/author' },
+    { name: 'Contact', link: '/contact' },
+  ];
   return (
-    <div>
-       <nav className='w-screen p-4 border-0 border-b-black flex justify-evenly '>
-          <div className=''>
-              Logo
-          </div>
-          <ul className='flex gap-3 text-black'>
-              <li className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>one </li>
-              <li>two </li>
-              <li>three </li>
-          </ul>
-          <div className=''>
-              last Hello
-          </div>
-       </nav>
-    </div>
+    <header>
+      <nav className='w-screen p-4 border-0 border-b-black flex justify-between items-center px-20 '>
+        <div className='font-bold text-2xl'>
+          Logo
+        </div>
+        <ul className='flex gap-6 text-black/70 font-semibold'>
+          {menuItems.map((item) => (
+            <li key={item.name} className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+        <div className='flex gap-3 '>
+          <span className='text-black/70 hover:text-black/100 transition-colors duration-200 ease-in-out cursor-pointer'><SearchIcon /></span>
+          <span className='text-black/70 hover:text-black/100 transition-colors duration-200 ease-in-out cursor-pointer'><ShoppingCart /></span>
+        </div>
+      </nav>
+    </header>
   )
 }
 
