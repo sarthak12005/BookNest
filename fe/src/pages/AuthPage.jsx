eaad00d3ea037044bfbd5811374163d9bf964ff5
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-10-07 21:40:42 +0530

    added some changes

--- fe/src/pages/AuthPage.jsx
+++ fe/src/pages/AuthPage.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const AuthPage = () => {
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className='h-screen flex justify-center items-center'>
+            {/* LEFT SIDE */}
+            <div className='w-1/2 bg-[#7BB5FB] h-full'>
+
+            </div>
+
+            {/* RIGHT SIDE */}
+            <div className='w-1/2 bg-gray-500 h-full'>
+
+            </div>
+            
         </div>
     )
 }
