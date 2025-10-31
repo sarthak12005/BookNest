6c2fbf18824ea72a158073cbb3b7272701cf9ed8
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-10-31 19:23:07 +0530

    added some changes

--- fe/src/lib/api.js
+++ fe/src/lib/api.js
@@ -3,27 +3,6 @@ import toast from 'react-hot-toast'
 
 
 
-
-
-export const fetchBooks = async () => {
-    try {
-        const response = await axiosInstance.get('/book/books');
-        const books = response.data;
-        return books;
-    } catch (error) {
-        console.error("Error fetching books:", error);
-
-    }
-};
-
-export const addBooks = async (data) => {
-    try {
-
-    } catch (error) {
-
-    }
-}
-
 export const fetchUser = async () => {
     try {
         const res = await axiosInstance.get('/auth/me');
@@ -56,6 +35,31 @@ export const loginUser = async (loginData, navigate) => {
     } catch (error) {
         console.error('Login error:', error);
         toast.error(error.response?.data?.message || "Login failed. Please try again.");
+    }
+}
+
+export const fetchBooks = async () => {
+    try {
+        const response = await axiosInstance.get('/book/books');
+        const books = response.data;
+        return books;
+    } catch (error) {
+        console.error("Error fetching books:", error);
+
+    }
+};
+
+export const addBook = async (data) => {
+    try {
+        const res = await axiosInstance.post('/book/book', data);
+
+        if (res.status === 200) {
+             toast.success("Book Added Successfully!");
+        }
+
+    } catch (error) {
+        console.log("error in adding book ", error);
+        toast.error("Error in adding book");
     }
 }
 
